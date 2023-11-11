4e3dd4185f6a6615d70157007dbc9309a44ac7e4
Ryan <sheffield.ryan@gmail.com>
2023-11-10 22:37:51 -0700

    We got a little dude

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,41 +7,57 @@ public class PlayerMovement : MonoBehaviour
 {
     public float movementSpeed = 3f;
     public float jumpSpeed = 1f;
-    public float fallGravity = 2.1f;
-    public float jumpGravity = 1.7f;
 
     private bool isGrounded = true;
     private Rigidbody rb;
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        int fakeVelocity = 0;
+
         if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.position += Vector3.right * movementSpeed * Time.deltaTime;
+            fakeVelocity += 1;
         }
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.position += Vector3.left * movementSpeed * Time.deltaTime;
+            fakeVelocity -= 1;
         }
+
+        // going right
+        if(fakeVelocity > 0)
+        {
+            transform.localRotation = Quaternion.Euler(0, 90, 0);
+        }
+        // going left
+        if (fakeVelocity < 0)
+        {
+            transform.localRotation = Quaternion.Euler(0, -90, 0);
+        }
+
+        animator.SetInteger("fakeVelocity", fakeVelocity);
 
         if (Input.GetKey(KeyCode.Space))
         {
             if (isGrounded)
             {
+                animator.SetTrigger("Jump");
                 rb.AddForce(Vector3.up * jumpSpeed);
                 isGrounded = false;
+                animator.SetBool("isGrounded", isGrounded);
             }
         }
-
-
     }
 
     void OnCollisionEnter(Collision collision)
@@ -49,6 +65,7 @@ public class PlayerMovement : MonoBehaviour
         if (rb.velocity.y <= 0)
         {
             isGrounded = true;
+            animator.SetBool("isGrounded", isGrounded);
         }
     }
 }
